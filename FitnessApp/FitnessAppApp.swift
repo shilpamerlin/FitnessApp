af3408cf75392d58a5ccfb103f79aa1b41fde7fc
Shilpa Merlin joy <shilpamjoy@gmail.com>
2024-08-29 16:44:16 -0400

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Shilpa Joy on 2024-08-29.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
